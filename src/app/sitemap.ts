9771ae463b2182f363d1493d6682ddfc0bfcf2d7
adelpro <adelpro@gmail.com>
2025-07-15 22:11:59 +0100

    fix: fix sitemap build error, converting to server componenet

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,50 +1,35 @@
-'use client';
+// No 'use client' here!
 
 import { MetadataRoute } from 'next';
-import { useEffect, useState } from 'react';
 
-import { Reciter } from '@/types';
 import { clientConfig } from '@/utils';
 import { getAllReciters } from '@/utils/api';
-export default function Sitemap(): MetadataRoute.Sitemap {
-  const [reciters, setReciters] = useState<Reciter[]>([]);
-  // Fetch reciters
-  useEffect(() => {
-    const loadReciters = async () => {
-      try {
-        const data = await getAllReciters();
-        setReciters(data);
-      } catch {}
-    };
-    loadReciters();
-  }, []);
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const reciters = await getAllReciters();
 
   const recitersMap = reciters.map((reciter) => ({
-    url: `/${clientConfig.APP_URL}/reciter/${reciter.id}}`,
+    url: `${clientConfig.APP_URL}/reciter/${reciter.id}`,
     lastModified: new Date(),
   }));
+
   return [
-    // Home Page
     {
       url: clientConfig.APP_URL,
       lastModified: new Date(),
     },
-    // About Page
     {
-      url: clientConfig.APP_URL + '/about',
+      url: `${clientConfig.APP_URL}/about`,
       lastModified: new Date(),
     },
-    // Contact Page
     {
-      url: clientConfig.APP_URL + '/contact',
+      url: `${clientConfig.APP_URL}/contact`,
       lastModified: new Date(),
     },
-    // Privacy Page
     {
-      url: clientConfig.APP_URL + '/privacy',
+      url: `${clientConfig.APP_URL}/privacy`,
       lastModified: new Date(),
     },
-
     ...recitersMap,
   ];
 }
