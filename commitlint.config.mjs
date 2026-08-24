026c613ea5e3fd75199c5c2c9332228c2530cfc6
mohamed sherif <mohamed.sherif.a.2022@gmail.com>
2026-08-24 17:39:30 +0300

    style: format codebase with prettier

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,20 +1,20 @@
 const config = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "feat", // New feature
-        "fix", // Bug fix
-        "refactor", // A code change that neither fixes a bug nor adds a feature
-        "revert", // Revert to a commit
-        "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-        "test", // Adding missing tests or correcting existing tests
-        "docs", // Documentation changes
-        "chore", // Changes to the build process or auxiliary tools and libraries such as documentation generation
-        "wip", // Work in progress (not ready for production)
-        "ci", // Changes to our CI configuration files and scripts
+        'feat', // New feature
+        'fix', // Bug fix
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'revert', // Revert to a commit
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        'test', // Adding missing tests or correcting existing tests
+        'docs', // Documentation changes
+        'chore', // Changes to the build process or auxiliary tools and libraries such as documentation generation
+        'wip', // Work in progress (not ready for production)
+        'ci', // Changes to our CI configuration files and scripts
       ],
     ],
   },
